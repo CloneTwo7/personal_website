1c9005f552e671f25b93e21bfd3b877bac8619f9
Travis Peterson <travisp0027@gmail.com>
2022-02-19 21:11:39 -0800

    updated eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,8 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./css/main.css');
     eleventyConfig.addPassthroughCopy('./images');
     eleventyConfig.addPassthroughCopy('./src');
-    
+    eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
@@ -15,4 +16,5 @@ module.exports = function(eleventyConfig) {
             output: "public"
         }
     };
+
 }
